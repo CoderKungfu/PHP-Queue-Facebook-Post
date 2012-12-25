cbf124b3a9908d26e1c1a1c152262cffb99bcb32
Michael Cheng <michael.cheng@mig33global.com>
2012-12-25 21:33:41 +0800

    Add Opauth test pages.

---
+++ src/htdocs/index.php
@@ -0,0 +1,30 @@
+<?php
+require_once dirname(__DIR__) . '/config.php';
+
+/**
+ * Opauth example
+ * 
+ * This is an example on how to instantiate Opauth
+ * For this example, Opauth config is loaded from a separate file: opauth.conf.php
+ * 
+ */
+
+/**
+ * Define paths
+ */
+define('CONF_FILE', dirname(__FILE__).'/'.'opauth.conf.php');
+
+/**
+* Load config
+*/
+if (!file_exists(CONF_FILE)){
+	trigger_error('Config file missing at '.CONF_FILE, E_USER_ERROR);
+	exit();
+}
+require CONF_FILE;
+
+/**
+ * Instantiate Opauth with the loaded config
+ */
+
+$Opauth = new Opauth( $config );
